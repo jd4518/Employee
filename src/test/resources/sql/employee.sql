dc01ae17e6999421a31ae002cb9f1468db5a9d98
jd4518 <formless_@naver.com>
2015-11-12 17:37:02 +0900

    Mysql City table select ->> Oracle City table insert
    mapper, factory
    sqlsession mybatis spring

--- src/test/resources/sql/employee.sql
+++ src/test/resources/sql/employee.sql
@@ -2,9 +2,28 @@ drop table city;
 
 create table city (
   id			 	 numeric(11),
-  name			 	 char(35),
+  name			 	 char(50),
   countrycode		 char(3),
-  district 			 char(20),
+  district 			 char(33),
   population 		 numeric(11),
   constraint city_pk_id primary key (id)
 )
+
+insert into city
+(
+	id,
+	name,
+	countrycode,
+	district,
+	population
+)
+values
+(
+	1,
+	'seoul',
+	'KOR',
+	'Asia',
+	99998
+)
+
+select * from city where countrycode='kor'
